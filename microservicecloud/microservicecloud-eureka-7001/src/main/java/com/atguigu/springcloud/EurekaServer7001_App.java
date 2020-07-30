431ce0edf3e0ba18268af5990fdb493ce2825993
james <yanmz_vip@163.com>
2020-07-30 14:17:37 +0800

    spring cloud 第一季学习

---
+++ microservicecloud/microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Tom
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7001_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001_App.class,args);
+    }
+}
---
+++ microservicecloud/microservicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,35 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.DeptDao;
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+    @Resource
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
